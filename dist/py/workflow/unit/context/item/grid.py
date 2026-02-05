d96f70ad9a932cc2dd9c1071518ed8eeda6be19b
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2026-02-05 16:34:16 +0200

    chore: improve dimensions

--- dist/py/workflow/unit/context/item/grid.py
+++ dist/py/workflow/unit/context/item/grid.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseModel, Field
 
@@ -22,7 +22,7 @@ class GridMetricType(Enum):
 
 
 class PointsGridDataProviderSchema(BaseModel):
-    dimensions: List[float] = Field(..., max_length=3, min_length=3)
+    dimensions: List[Union[float, str]] = Field(..., max_length=3, min_length=3)
     shifts: Optional[List[float]] = Field(None, max_length=3, min_length=3)
     reciprocalVectorRatios: Optional[List[float]] = Field(None, max_length=3, min_length=3)
     gridMetricType: GridMetricType
