d0fcfdadf7361cbba1ef5f8ee9ac71de66787ccd
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-04-25 10:14:58 +0800

    upgrade typescript to v5; webapp, ade uses typscript v5

--- dist/js/types.js
+++ dist/js/types.js
@@ -29,7 +29,7 @@ var Name;
     Name["VASPInputDataManager"] = "VASPInputDataManager";
     Name["VASPNEBInputDataManager"] = "VASPNEBInputDataManager";
     Name["NWChemInputDataManager"] = "NWChemInputDataManager";
-})(Name = exports.Name || (exports.Name = {}));
+})(Name || (exports.Name = Name = {}));
 /**
  * This interface was referenced by `ContextProvidersDirectoryEnum`'s JSON-Schema
  * via the `definition` "ContextProviderNameEnum".
@@ -61,7 +61,7 @@ var ContextProviderNameEnum;
     ContextProviderNameEnum["VASPInputDataManager"] = "VASPInputDataManager";
     ContextProviderNameEnum["VASPNEBInputDataManager"] = "VASPNEBInputDataManager";
     ContextProviderNameEnum["NWChemInputDataManager"] = "NWChemInputDataManager";
-})(ContextProviderNameEnum = exports.ContextProviderNameEnum || (exports.ContextProviderNameEnum = {}));
+})(ContextProviderNameEnum || (exports.ContextProviderNameEnum = ContextProviderNameEnum = {}));
 /**
  * The type of the message to distinguish the direction of the message.
  */
@@ -69,7 +69,7 @@ var Type;
 (function (Type) {
     Type["fromIframeToHost"] = "from-iframe-to-host";
     Type["fromHostToIframe"] = "from-host-to-iframe";
-})(Type = exports.Type || (exports.Type = {}));
+})(Type || (exports.Type = Type = {}));
 /**
  * The action to be performed upon receiving the message.
  */
@@ -78,7 +78,7 @@ var Action;
     Action["setData"] = "set-data";
     Action["getData"] = "get-data";
     Action["info"] = "info";
-})(Action = exports.Action || (exports.Action = {}));
+})(Action || (exports.Action = Action = {}));
 /**
  * This interface was referenced by `WorkflowSubworkflowConvergenceEnumOptions`'s JSON-Schema
  * via the `definition` "ConvergenceParameterNameEnum".
@@ -88,4 +88,4 @@ var ConvergenceParameterNameEnum;
     ConvergenceParameterNameEnum["N_k"] = "N_k";
     ConvergenceParameterNameEnum["N_k_nonuniform"] = "N_k_nonuniform";
     ConvergenceParameterNameEnum["N_k_nonuniform_2D"] = "N_k_nonuniform_2D";
-})(ConvergenceParameterNameEnum = exports.ConvergenceParameterNameEnum || (exports.ConvergenceParameterNameEnum = {}));
+})(ConvergenceParameterNameEnum || (exports.ConvergenceParameterNameEnum = ConvergenceParameterNameEnum = {}));
